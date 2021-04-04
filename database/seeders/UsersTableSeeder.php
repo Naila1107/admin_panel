5594424df4188c5e567b199a7e266a467562ef70
dunya <dunyayousufzai@gamil.com>
2021-04-04 12:21:26 +0430

    admin-panle
    updated

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+namespace Database\Seeders;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'id' => 1,
+            'name' => 'naila',
+            'email' => 'naila@gmail.com',
+            'password' => Hash::make('naila123'),
+        ]);
+    }
+}
